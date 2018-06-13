ac2d7bc9491f22c3943ef74b4bdfea85b66ed758
Kevin Cheng <myth282429096@163.com>
2018-06-14 00:48:49 +0800

    Spring 之旅   AOP 支持 常用上下文实例

---
+++ chapter1/src/main/java/com/myth/KnightMinsterMain.java
@@ -0,0 +1,19 @@
+package com.myth;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.ui.context.support.UiApplicationContextUtils;
+
+public class KnightMinsterMain {
+
+    public static void main(String[] args) {
+        //使用FilePath绝对路径得到上下文
+//        ApplicationContext context = new FileSystemXmlApplicationContext("D:/IdeaProjects/springInAction/chapter1/src/main/resources/Minstrel.xml");
+        //使用类路径找到应用上下文
+        ApplicationContext context = new ClassPathXmlApplicationContext("Minstrel.xml");
+        Knight braveKnight = context.getBean(Knight.class);
+        braveKnight.embarkOnQuest();
+    }
+}
