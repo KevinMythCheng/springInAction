5ce684a2aa9a8f030f93c1dd834847fdf97287a7
Kevin Cheng <myth282429096@163.com>
2018-07-01 20:06:42 +0800

    使用XML 注入的方式 constructor-arg  或者C命名空间

---
+++ chapter2/src/main/java/com/myth/soundsystem/impl/BlankDisc.java
@@ -0,0 +1,18 @@
+package com.myth.soundsystem.impl;
+
+import com.myth.soundsystem.CompactDisc;
+
+public class BlankDisc implements CompactDisc {
+    private String title;
+    private String artist;
+
+    public BlankDisc(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing " + title + " by " + artist);
+    }
+}
